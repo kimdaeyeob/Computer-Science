4114d0467e7c7de655baa648ea24f082002f5bb9
kimdaeyeobbb <kimdyk2@hanmail.net>
2025-08-28 08:28:14 +0900

    add: Adds basic C and Java examples
    Adds initial examples for C and Java languages.
    
    This commit introduces fundamental "Hello, World!" programs in Java and a simple C program that demonstrates the logical NOT operator.
    These examples serve as starting points for exploring the languages.

---
+++ language/c_c++/c_language/test1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    int c = 12;
+    printf("!c값 확인 %d\n", !c);
+    return 0;
+}
---
+++ language/java/java-start/src/variable/VarEx3.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num1=10000000000L;
+        boolean isTrue=true;
+        System.out.println(num1);
+        System.out.println(isTrue);
+    }
+}
