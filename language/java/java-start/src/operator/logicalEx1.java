4cd34b322a4318d2633d8ff8465058b718e12f76
kimdaeyeobbb <kimdyk2@hanmail.net>
2025-08-28 08:36:29 +0900

    add: Adds operator examples
    Adds several examples demonstrating different operator usages in Java, including logical and arithmetic operations.
    These examples showcase basic calculations and conditional logic.

---
+++ language/java/java-start/src/operator/logicalEx1.java
@@ -0,0 +1,8 @@
+package operator;
+
+public class logicalEx1 {
+    public static void main(String[] args) {
+       int a = 15;
+        System.out.println(a>10 && a<20 ? "true" : "false");
+    }
+}
---
+++ language/java/java-start/src/operator/operationEx3.java
@@ -0,0 +1,8 @@
+package operator;
+
+public class operationEx3 {
+    public static void main(String[] args) {
+        int score= 85;
+        System.out.println(80 <= score && score <=100);
+    }
+}
