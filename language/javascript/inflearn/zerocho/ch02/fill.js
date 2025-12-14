d6802c1d1b6d4ce88c89a1bb58fb30d39a0dd708
kimdaeyeobbb <kimdyk2@hanmail.net>
2025-12-15 07:25:04 +0900

    feat: Adds examples demonstrating the use of Array.fill() and Array.flat() methods in JavaScript

---
+++ language/javascript/inflearn/zerocho/ch02/fill.js
@@ -0,0 +1,4 @@
+const emptyArr = Array(5);
+console.log(emptyArr); // [ <5 empty items> ]
+console.log(emptyArr.fill()); // [ undefined, undefined, undefined, undefined, undefined ]
+console.log(emptyArr.fill(0)); // [ 0, 0, 0, 0, 0 ]
