1ffe2db93a64c03ca370eb30038facf29de8d051
kimdaeyeobbb <kimdyk2@hanmail.net>
2026-03-05 09:51:16 +0900

    Adds example for TypeScript literal types
    Illustrates the concept of primitive types and their relation to literal values.
    Demonstrates how to declare variables using union types with string, number, and null literals, showcasing valid and type-checked assignments.

---
+++ language/typescript/learning-typescript/ch03/literal1.ts
@@ -0,0 +1,11 @@
+/**
+ * * 각 원시타입은 해당 타입이 가질 수 있는 가능한 모든 리터럴 값의 전체 조합
+ * ! 원시 타입은 해당 타입의 가능한 모든 리터럴 값의 집합
+ */
+const philosopher = "Aristotle";
+
+let certainType: null | number | "kream";
+certainType = null;
+certainType = 11;
+certainType = "kream";
+certainType = "certainString";
