02c55ab8ce7456bbce0d8940f81d57c7ce82e672
kimdaeyeobbb <kimdyk2@hanmail.net>
2026-03-19 08:24:12 +0900

    Adds mouse event listeners for mousedown, mousemove, and mouseup events

---
+++ language/javascript/inflearn/zerocho/ch04/event/mouseEvent1.js
@@ -0,0 +1,11 @@
+window.addEventListener("mousedown", (e) => {
+  console.log("mouse down: ", e);
+});
+
+window.addEventListener("mousemove: ", (e) => {
+  console.log("mouse move: ", e);
+});
+
+window.addEventListener("mouseup", (e) => {
+  console.log("mouse up: ", e);
+});
