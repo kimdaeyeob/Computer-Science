28904d45248a8050d3d637e69a53517edf6ab840
kimdaeyeobbb <kimdyk2@hanmail.net>
2026-03-19 09:00:09 +0900

    Adds HTML structure and JavaScript event listeners for table interactions

---
+++ language/javascript/inflearn/zerocho/ch04/event/eventbubbling.js
@@ -0,0 +1,11 @@
+document.querySelector("td").addEventListener("click", () => {
+  console.log("td를 클릭함");
+});
+
+document.querySelector("tr").addEventListener("click", () => {
+  console.log("tr을 클릭하였습니다");
+});
+
+document.querySelector("table").addEventListener("click", () => {
+  console.log("table을 클릭함");
+});
